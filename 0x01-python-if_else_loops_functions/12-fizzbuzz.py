a88928d0e9573d9942bee67caa9db4cab6c927c3
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-10-31 19:53:21 -0700

    Python If Else Loops

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,23 +5,12 @@ def fizzbuzz():
     for i in number:
         fizz = i % 3
         buzz = i % 5
-        if i < 100:
-            if fizz == 0 and buzz != 0:
-                print("Fizz", end=' ')
-            elif buzz == 0 and fizz != 0:
-                print("Buzz", end=' ')
-            elif fizz == 0 and buzz == 0:
-                print("FizzBuzz", end=' ')
-            else:
-                print(i, end=' ')
+        if fizz == 0 and buzz != 0:
+            print("Fizz", end=' ')
+        elif buzz == 0 and fizz != 0:
+            print("Buzz", end=' ')
+        elif fizz == 0 and buzz == 0:
+            print("FizzBuzz", end=' ')
         else:
-            if fizz == 0 and buzz != 0:
-                print("Fizz", end='')
-            elif buzz == 0 and fizz != 0:
-                print("Buzz", end='')
-            elif fizz == 0 and buzz == 0:
-                print("Fizzbuzz", end='')
-            else:
-                print(i, end='')
-    return i
-            
+            print(i, end=' ')
+    return i   
