ce9969b45c6f7ef6f1e94d5eb608ffd200a699a9
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-05 20:08:46 +0000

    Python Imports

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def element_at(my_list, idx):
     n = len(my_list)
-    result = my_list[idx]
     if idx >= n or idx < 0:
         return None
     else:
+        result = my_list[idx]
         return result
