392950c6b99a3e5ebc63f084d6bc2b3956990618
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-05 20:52:14 +0000

    Python data structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,5 +7,5 @@ def replace_in_list(my_list, idx, element):
         my_list
         return my_list
     else:
-        my_list.insert(idx, element)
+        my_list[idx] = element
         return my_list
