86f6a7bf11ec4427246fa67fb1182b3f0d58b66b
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-06 23:13:09 +0000

    Python Import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,16 +7,16 @@ argv_count = (len(sys.argv) - 1)
 
 # if no argument was passed
 if argv_count == 0:
-    print(f"{argv_count} arguments.")
+    print("{:d} arguments.".format(argv_count))
 
 # if exactly one argument was passed
 elif argv_count == 1:
     print(f"{argv_count} argument:")
     for num in range(1, argv_count + 1):
-        print(f"{num}: {sys.argv[num]}")
+        print("{:d}: {:s]}".format(num, sys.argv[num]))
 
 # if arguments are more than 1
 else:
     print(f"{argv_count} arguments:")
     for num in range(1, argv_count + 1):
-        print(f"{num}: {sys.argv[num]}")
+        print("{:d}: {:s}".format(num, sys.argv[num]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -8,10 +8,10 @@ result = 0
 
 # if no arguments are passed
 if argv_count == 0:
-    print(f"{argv_count}")
+    print("{:d}".format(argv_count))
 
 # if an argument or more is passed
 else:
     for num in range(1, argv_count + 1):
         result += int(sys.argv[num])
-    print(result)
+    print("{:d}".format(result))
