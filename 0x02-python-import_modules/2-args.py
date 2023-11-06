60ac07dc1302fae5eb2cfaa92e5471dd34e6cb3e
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-06 23:20:07 +0000

    Python Imports

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -13,10 +13,10 @@ if argv_count == 0:
 elif argv_count == 1:
     print(f"{argv_count} argument:")
     for num in range(1, argv_count + 1):
-        print("{:d}: {:s]}".format(num, sys.argv[num]))
+        print("{}: {]}".format(num, sys.argv[num]))
 
 # if arguments are more than 1
 else:
     print(f"{argv_count} arguments:")
     for num in range(1, argv_count + 1):
-        print("{:d}: {:s}".format(num, sys.argv[num]))
+        print("{}: {}".format(num, sys.argv[num]))
