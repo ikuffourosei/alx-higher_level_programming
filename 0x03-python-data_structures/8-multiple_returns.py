09605c6f8a0b5e7cc1c80978c153b4acab4fd52d
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-06 03:15:51 +0000

    Python data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@
 def multiple_returns(sentence):
     s = len(sentence)
     if sentence == "":
-        sentence[0] = None
+        result = (0, None)
     result = (s, sentence[0])
     return result
