7f3f70a732807d0cabe1f42de8df54d0d39657d0
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-06 23:23:32 +0000

    Python Imports

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import sys
+    # length of arguments
+    argv_count = (len(sys.argv) - 1)
 
-# length of arguments
-argv_count = (len(sys.argv) - 1)
+    # if no argument was passed
+    if argv_count == 0:
+        print("{:d} arguments.".format(argv_count))
+        
+    # if exactly one argument was passed
+    elif argv_count == 1:
+        print(f"{argv_count} argument:")
+        for num in range(1, argv_count + 1):
+            print("{}: {]}".format(num, sys.argv[num]))
 
-# if no argument was passed
-if argv_count == 0:
-    print("{:d} arguments.".format(argv_count))
-
-# if exactly one argument was passed
-elif argv_count == 1:
-    print(f"{argv_count} argument:")
-    for num in range(1, argv_count + 1):
-        print("{}: {]}".format(num, sys.argv[num]))
-
-# if arguments are more than 1
-else:
-    print(f"{argv_count} arguments:")
-    for num in range(1, argv_count + 1):
-        print("{}: {}".format(num, sys.argv[num]))
+        # if arguments are more than 1
+    else:
+        print(f"{argv_count} arguments:")
+        for num in range(1, argv_count + 1):
+            print("{}: {}".format(num, sys.argv[num]))
