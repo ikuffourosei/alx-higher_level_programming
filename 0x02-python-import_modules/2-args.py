54b868f1d830cfb8cef97c58295225ae99156c77
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-06 23:30:12 +0000

    Python Import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(argv_count))
     # if exactly one argument was passed
     elif argv_count == 1:
-        print(f"{argv_count} argument:")
+        print(f"{:d} argument:".format(argv_count))
         for num in range(1, argv_count + 1):
-            print("{}: {]}".format(num, sys.argv[num]))
+            print("{}: {]".format(num, sys.argv[num]))
 
         # if arguments are more than 1
     else:
