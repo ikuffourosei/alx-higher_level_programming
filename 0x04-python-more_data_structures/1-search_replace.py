a34de7d0d36924b7d28543dfc2a8947fe25611fc
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-09 19:08:19 +0000

    More Data Structures

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    list_len = len(my_list)
+    new_list = []
+    for i in range(list_len):
+        if my_list[i] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
+    return new_list
