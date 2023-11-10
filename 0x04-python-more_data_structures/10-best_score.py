941415ffb096bc610467b443ce2c77f36b783540
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-10 02:32:21 +0000

    More Data Structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,7 +7,7 @@ def best_score(a_dictionary):
         return None
     # get listof values and find max
     maxx = max(a_dictionary.values())
-    for key,val in a_dictionary.items():
+    for key, val in a_dictionary.items():
         if val == maxx:
             return key
     return None
