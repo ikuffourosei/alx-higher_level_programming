cbcfdfc07e6cdf45ba670c3afb40fe6c01ec8220
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-21 20:43:06 +0000

    Classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+class Square:
+    
+    """
+    A class Square that defines a Square with a private size attribute
+
+    Attributes: 
+              __size (int)
+
+    """
+    def __init__(self, size):
+
+        """
+        Initializing the class
+
+        Args: 
+             size (int) - size of the square
+
+        Return: None
+
+        """
+        self.__size = size
