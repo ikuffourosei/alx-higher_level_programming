b017cff34727df3d07d12909313934efd0e9a182
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-11-21 21:31:17 +0000

    Classess

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,17 +7,13 @@ class Square:
 
     Attributes:
         __size (int): size of the square
+
     """
     def __init__(self, size=0):
         """Instantiation with optional size
 
         Args:
             size (int): size of the square
-        Returns:
-            None
-        Raises:
-            TypeError: If the type of the size is not an int
-            ValueError: if size is less than 0, then the sqaure is invalid
         """
         if size < 0:
             raise ValueError("size must be >= 0")
