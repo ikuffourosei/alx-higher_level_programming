b48cad2921dbcd68ce3b5ebc22fde3961c362511
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-12-01 17:01:48 +0000

    Python test Driven Development

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+"""Unittest Case for 6-max_integer module
+"""
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 4, 7]), 7)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([-2, -4, -7]), -2)
+        self.assertEqual(max_integer([0]), 0)
+        self.assertIsInstance([1, 3, 5], list)
+        self.assertNotEqual([1, 5, 3], 1)
+        self.assertRaises(TypeError, max_integer, dict)
+        self.assertRaises(TypeError, max_integer, tuple)
+        self.assertRaises(TypeError, max_integer, set)
+        self.assertRaises(TypeError, max_integer, str)
+        self.assertRaises(TypeError, max_integer, bool)
+        self.assertRaises(TypeError, max_integer, [1, "2"])
+
+if __name__ == '__main__':
+    unittest.main()
