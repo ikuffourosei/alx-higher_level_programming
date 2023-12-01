e170c31d5596d6aec59097299f63c840b73800b8
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-12-01 17:17:18 +0000

    Python test Driven Development

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,16 +7,38 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
+        """Testing the function with integer list contents
+        """
         self.assertEqual(max_integer([1, 4, 7]), 7)
-        self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([-2, -4, -7]), -2)
         self.assertEqual(max_integer([0]), 0)
+
+    def tes_max_integer(self):
+        """Testing with empty list passed as argument
+        """
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_integer_raises(self):
+        """Testing the function with different data types passed as arguments
+        """
         self.assertRaises(TypeError, max_integer, dict)
         self.assertRaises(TypeError, max_integer, tuple)
         self.assertRaises(TypeError, max_integer, set)
         self.assertRaises(TypeError, max_integer, str)
         self.assertRaises(TypeError, max_integer, bool)
-        self.assertRaises(TypeError, max_integer, [1, "2"])
+
+        #Testing with list containing different data type
+        mixed = ["1", 77, True]
+        with self.assertRaises(TypeError):
+            max_integer(mixed)
+
+    def test_max_integer_duplicate(self):
+        self.assertEqual(max_integer([2, 2, 2, 2]), 2)
+
+    def test_max_integer_large_list(self):
+        """Testing the function with large list
+        """
+        self.assertEqual(max_integer([x for x in range(10000)]), 9999)
 
 if __name__ == '__main__':
     unittest.main()
