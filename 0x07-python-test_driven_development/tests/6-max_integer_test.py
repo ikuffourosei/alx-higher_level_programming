3aa6bf92a0b5b6ba6d399c67c63fd05bd69c8a49
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-12-01 17:04:10 +0000

    Python test Driven Development

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,8 +11,6 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([-2, -4, -7]), -2)
         self.assertEqual(max_integer([0]), 0)
-        self.assertIsInstance([1, 3, 5], list)
-        self.assertNotEqual([1, 5, 3], 1)
         self.assertRaises(TypeError, max_integer, dict)
         self.assertRaises(TypeError, max_integer, tuple)
         self.assertRaises(TypeError, max_integer, set)
