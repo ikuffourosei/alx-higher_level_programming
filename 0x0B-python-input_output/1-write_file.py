ff328aba0192d2e7a9d22c876a3fc82c10804095
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-12-05 23:34:54 +0000

    Write a text to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,10 @@
 
 """The write file module
 """
+
+
 def write_file(filename="", text=""):
+    """A function that writes a text to a string"""
     with open(filename, "w", encoding="UTF-8") as wf:
         file_write = wf.write(text)
-    return file_write
+        return file_write
