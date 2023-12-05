311aafbe3b1991f259a58b2232acdd9aacf4c9f4
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-12-05 23:20:19 +0000

    Read a file and print to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ The read_file module
 
 def read_file(filename=''):
     """Reads a file and prints to stdout"""
-    with open(filename, "r", enoding="UTF-8") as rf:
+    with open(filename, "r", encoding="UTF-8") as rf:
         print(rf.read())
