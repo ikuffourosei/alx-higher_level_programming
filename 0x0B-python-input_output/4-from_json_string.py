f5d30dd28649d840a6be1a2e6752e5c692d1c821
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-12-05 23:54:00 +0000

    Convert JSON data format to the original data structure

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr.bin/python3
+"""The ''from_json_string'' module
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """function that converts JSON format data to the original data structure
+    """
+    return json.loads(my_str)
