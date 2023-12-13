6ac7c6049d78e9bea1da4de742f9af19989e5c68
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-12-13 20:51:55 +0000

    Square: class constructor. updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,10 +12,6 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
-        self.x = x
-        self.y = y
-        self.id = id
 
     def __str__(self):
         """overriding the '__str__'method"""
