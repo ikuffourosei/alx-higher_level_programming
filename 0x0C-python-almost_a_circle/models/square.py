a5cdb7909dbde6c899b29237211ec1bd5fbb08ae
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-12-13 20:45:43 +0000

    Square: class constructor

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,11 +12,11 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size =  size
+        self.size = size
         self.x = x
         self.y = y
         self.id = id
-    
+
     def __str__(self):
-            """overriding the '__str__'method"""
-            return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        """overriding the '__str__'method"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
