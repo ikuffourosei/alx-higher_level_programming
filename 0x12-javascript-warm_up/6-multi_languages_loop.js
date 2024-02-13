d8b1ebc7994bb15d66b4b38ae7cc4067ba6f7188
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-02-13 22:55:14 +0000

    js warm up

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+const arr = [firstLine, secondLine, thirdLine];
+for (let ind = 0; ind < arr.length; ind++) {
+  console.log(arr[ind]);
+}
