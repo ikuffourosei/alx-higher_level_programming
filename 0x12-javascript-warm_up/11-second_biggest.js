96eb8726056c22d242c90a180b45d1dabfebc235
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-02-14 03:09:24 +0000

    js warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,3 +2,13 @@
 const processArg = require('process');
 const arg = processArg.argv;
 
+if (arg.length <= 3) {
+  console.log(0);
+} else {
+  let newList = [];
+  for (let index = 2; index < arg.length; index++) {
+    newList.push(arg[index]);
+  }
+  newList = newList.sort().reverse();
+  console.log(newList[1]);
+}
