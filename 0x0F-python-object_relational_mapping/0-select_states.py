94a87bf8500133ad8b4c71e3e4cb63f0d7d11a6a
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-02-16 22:52:04 +0000

    order states by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-import MySQLdb as msdb
+import MySQLdb
 import sys
 
 def execution():
-    
+    """Function to execute the query
+    Creates a connection to the base, executes the query, closes the cursor
+    and database and handles operationalError
+    """
     if len(sys.argv) == 4:
         arg = sys.argv
         username, password, database = arg[1:]
         #creating a connection to a database called hbtn_0e_0_usa
-        db = msdb.Connect(password=password, user=username, database=database)
+        db = MySQLdb.Connect(password=password, user=username, database=database)
 
         # cursor
         try:
             cur = db.cursor()
             cur.execute("SELECT * FROM states ORDER BY id")
             state = cur.fetchall()
-        except msdb.OperationalError as e:
+        except MySQLdb.OperationalError as e:
             if e[0] == 1060:
                 pass
             else:
