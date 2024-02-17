2d683e429fe2000b7c83eb7552a75d0da2f9eee6
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-02-17 21:45:09 +0000

    joining two table fields

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+cli = sys.argv
+
+
+def connection():
+    if len(cli) == 4:
+        username, password, databse = cli[1:]
+        db = MySQLdb.connect(user=username, password=password,
+                             database=databse)
+        try:
+            cur = db.cursor()
+            cur.execute("""SELECT cities.id, cities.name, states.name
+                        FROM cities JOIN states
+                        ON cities.state_id=states.id
+                        ORDER BY cities.id""")
+            allCites = cur.fetchall()
+        except MySQLdb.OperationalError as e:
+            if e[0] == 1060:
+                pass
+            else:
+                raise
+        for items in allCites:
+            print(str(items))
+
+        # cleaning up
+        cur.close()
+        db.close()
+    else:
+        print(f"Usage: <python script> username, password, database")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    connection()
