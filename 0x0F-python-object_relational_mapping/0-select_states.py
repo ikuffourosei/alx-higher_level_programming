37a1ffd3900f151ea4a6394b47647c924e6f2867
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-02-18 19:42:21 +0000

    fixed bug

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,6 +2,7 @@
 import MySQLdb
 import sys
 
+
 def execution():
     """Function to execute the query
     Creates a connection to the base, executes the query, closes the cursor
@@ -11,7 +12,10 @@ def execution():
         arg = sys.argv
         username, password, database = arg[1:]
         # creating a connection to a database called hbtn_0e_0_usa
-        db = MySQLdb.Connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.Connect(password=password,
+                             user=username, database=database,
+                             host='localhost',
+                             port=3306)
 
         # cursor
         try:
@@ -19,18 +23,19 @@ def execution():
             cur.execute("SELECT * FROM states ORDER BY id")
             state = cur.fetchall()
         except MySQLdb.OperationalError as e:
-            print("Error:", e)
-            sys.exit(1)
-        
+            if e[0] == 1060:
+                pass
+            else:
+                raise
         for row in state:
             print(str(row))
-        
         # cleaning up
         cur.close()
         db.close()
     else:
         print("Usage: <python script> username, password, database")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     execution()
