acfea114c555bac8a55aba7d1930eb605d47ac5a
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-02-18 21:16:42 +0000

    venv

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-""" a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument
-"""
+'''
+Script that lists all states from the database
+'''
 
 
 import MySQLdb
-import sys
-
-
-def connection():
-    """Function that displays the result of a query where query is user input
-    """
-    cli = sys.argv
-    if len(cli) == 5:
-        username, password, database, name = cli[1:]
-
-        db = MySQLdb.connect(port=3306, user=username, host='localhost',
-                             password=password, database=database)
-        cur = db.cursor()
-        cur.execute("""SELECT * FROM states WHERE name LIKE
-                    '{:s}' ORDER BY id ASC""".format(name))
-        user_select = cur.fetchall()
-        if user_select:
-            print(user_select[0])
-        else:
-            print(f"'{name}' not found in state")
-    else:
-        print("\n").join(["Add input value of the name of state to search for",
-                          "Usage: <python script> username, password, database"
-                          ])
-        sys.exit(1)
+from sys import argv
 
 
 if __name__ == "__main__":
-    connection()
+    cont = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        password=argv[2], database=argv[3])
+    cursor = cont.cursor()
+    cursor.execute(
+            "SELECT * FROM states WHERE name LIKE"
+            " '{:s}' ORDER BY id ASC".format(argv[4]))
+    db = cursor.fetchall()
+    for i in db:
+        if i[1] == argv[4]:
+            print(i)
