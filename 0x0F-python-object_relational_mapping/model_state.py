c27f03b5d171742f8963cc30eb0881ea06e8ac8f
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-02-20 15:39:58 +0000

    sql alchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a python file that contains the class definition of a State
+and an instance Base = declarative_base():
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Sequence
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, Sequence('states_id_seq'), primary_key=True)
+    name = Column(String(128), nullable=False)
