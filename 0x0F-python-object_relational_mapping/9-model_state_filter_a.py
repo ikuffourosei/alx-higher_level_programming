9b09eeb0b3635ba5777d034d0e1f24d87b7b3180
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-02-20 21:29:45 +0000

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script that lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username, passwd, dbs = argv[1:]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, passwd, dbs))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    a = session.query(State).order_by(State.id).filter(State.name.like('%a%'))
+    for rows in a:
+        print(f"{rows.id}:{rows.name}")
+    session.close()
