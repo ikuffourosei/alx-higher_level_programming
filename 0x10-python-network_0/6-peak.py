0e55e710f98af33679c9b65b1bee2f1ab87db86a
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-04 01:43:09 +0000

    finding the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
 def find_peak(list_of_integers):
+    """finds the peak of a list of integers"""
     if not list_of_integers:
         return None
 
