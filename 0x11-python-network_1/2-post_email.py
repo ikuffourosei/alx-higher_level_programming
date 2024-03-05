acff221d3f9c3795a4b988849971be6d78613895
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-05 21:25:34 +0000

    adding email parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,5 +6,13 @@ and displays the body of the response (decoded in utf-8)"""
 
 import sys
 import urllib.request
+import urllib.parse
 
-url = sys
+url = sys.argv[1]
+mail = {"email": sys.argv[2]}
+data = urllib.parse.urlencode(mail)
+data = data.encode("ascii")
+req =  urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as request:
+    body = request.read()
+    print(f"{body.decode('utf-8')}")
