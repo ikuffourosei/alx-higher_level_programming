b810c3b044cf60d798aea736aa5e08b4037bd777
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-05 21:01:10 +0000

    get headers

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response."""
+
+
+import sys
+import urllib.request
+
+
+url = sys.argv[1]
+req = urllib.request.Request('{}'.format(url))
+with urllib.request.urlopen(req) as page:
+    resp = page.getheader('X-Request-Id')
+    print(resp)
