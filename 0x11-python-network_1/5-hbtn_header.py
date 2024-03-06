573734383618cc358caf682da9b2308445e3b37a
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-06 02:24:20 +0000

    get headers using requests library

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a Python script that displays the value of the X-Request-Id
+Usage: ./5-hbtn_header <url> 
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
