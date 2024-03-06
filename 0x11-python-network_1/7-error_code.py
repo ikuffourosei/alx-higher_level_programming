450e4160dce846ff16f94fbecf758211a99b3f1f
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-06 02:43:21 +0000

    display error code with requests library

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,3 +11,5 @@ if __name__ == "__main__":
     response = requests.get(url)
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
