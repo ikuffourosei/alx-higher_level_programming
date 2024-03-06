d8c084d99da46ea1aff2dfc4335f4cc4e0a2ca5e
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-06 01:58:27 +0000

    display error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+  - Handles HTTP errors.
+"""
+import sys
+import urllib.request
+import urllib.error
+
+
+url = sys.argv[1]
+
+request = urllib.request.Request(url)
+try:
+    with urllib.request.urlopen(request) as resp:
+        print(resp.read().decode("ascii"))
+except urllib.error.HTTPError as e:
+    print(f"Error code: {e.code}")
