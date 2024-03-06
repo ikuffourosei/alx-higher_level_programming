4ee8b29622f92b3f47cc729705a0bbe8729f763e
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-06 02:12:13 +0000

    fetching url using requests library

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     response = requests.get(url)
     print("Body response:")
     print(f"\t- type: {type(response.text)}")
-    print(f"\t- Content: {response.text}")
+    print(f"\t- content: {response.text}")
